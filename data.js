b44d7567896e87cdf258bef13666afdab2950b8a
artechinteriors2012-blip <artechinteriors2012@gmail.com>
2025-08-25 18:59:39 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,289 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-hall16k",
+      "name": "1 HALL.16K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.7524438492762204,
+        "pitch": 0.1768665959338449,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4066245197033425,
+          "pitch": 0.10141872606111946,
+          "rotation": 10.995574287564278,
+          "target": "5-4-kids-bed"
+        },
+        {
+          "yaw": -0.0871265175153173,
+          "pitch": -0.011902323086591338,
+          "rotation": 4.71238898038469,
+          "target": "4-5-gust-room"
+        },
+        {
+          "yaw": 0.2639493935387307,
+          "pitch": 0.037998356644173725,
+          "rotation": 0,
+          "target": "3-06-kitchen-16k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-3-master-bed",
+      "name": "3 MASTER BED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4253619383617187,
+        "pitch": -0.06465682184835231,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.181986217496764,
+          "pitch": 0.033080092098483505,
+          "rotation": 4.71238898038469,
+          "target": "0-1-hall16k"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-2-dinning",
+      "name": "2 DINNING",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3873350826769322,
+        "pitch": 0.0981241335527745,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0709418978056506,
+          "pitch": 0.2166428663068185,
+          "rotation": 0,
+          "target": "3-06-kitchen-16k"
+        },
+        {
+          "yaw": 2.022177311419152,
+          "pitch": 0.17658564000033827,
+          "rotation": 0,
+          "target": "4-5-gust-room"
+        },
+        {
+          "yaw": 1.313559329568779,
+          "pitch": 0.14330936520832793,
+          "rotation": 0,
+          "target": "0-1-hall16k"
+        },
+        {
+          "yaw": 1.0400213497703934,
+          "pitch": 0.05853395479779877,
+          "rotation": 0,
+          "target": "5-4-kids-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-06-kitchen-16k",
+      "name": "06 KITCHEN 16K",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -0.28091465543605665,
+        "pitch": -0.11360800360554002,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.485166039796943,
+          "pitch": 0.17377082613843697,
+          "rotation": 3.141592653589793,
+          "target": "2-2-dinning"
+        },
+        {
+          "yaw": -3.0396791748066345,
+          "pitch": 0.06970651994676658,
+          "rotation": 17.27875959474387,
+          "target": "1-3-master-bed"
+        },
+        {
+          "yaw": -2.537026005608956,
+          "pitch": 0.054940201260176735,
+          "rotation": 0,
+          "target": "5-4-kids-bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5-gust-room",
+      "name": "5 GUST ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.0679108687227998,
+        "pitch": 0.1200280115602741,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9584966864965061,
+          "pitch": 0.10857009997758738,
+          "rotation": 4.71238898038469,
+          "target": "2-2-dinning"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-4-kids-bed",
+      "name": "4 KIDS BED",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3274580993959901,
+        "pitch": 0.14158647593857054,
+        "fov": 1.3446888170112152
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0569388777024393,
+          "pitch": 0.11682416302230614,
+          "rotation": 10.995574287564278,
+          "target": "0-1-hall16k"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
